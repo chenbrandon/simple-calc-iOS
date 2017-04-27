ddba4859f59f9aed1eb68c66f5770ad761e7d930
Brandon Chen <chenb777@uw.edu>
2017-04-26 20:50:15 -0700

    connect historyviewcontroller to view

---
+++ simple-calc/HistoryViewController.swift
@@ -0,0 +1,39 @@
+//
+//  HistoryViewController.swift
+//  simple-calc
+//
+//  Created by Brandon Chen on 4/26/17.
+//  Copyright © 2017 Brandon Chen. All rights reserved.
+//
+
+import UIKit
+
+class HistoryViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBOutlet weak var list: UIScrollView!
+    
+    var data: [String] = []
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
